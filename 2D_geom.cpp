d1544f4e7ace4ff0d9cb70160a3cb0e6581fd801
Sameersingh29 <73021790+Sameersingh29@users.noreply.github.com>
2024-07-02 22:15:07 +0530

    Create 2D_geom.cpp

---
+++ 2D_geom.cpp
@@ -0,0 +1,68 @@
+#include<GL/glut.h>
+#include<cmath>
+
+float tri[3][3] = {{-100,100,0},{-100,-100,100},{1,1,1}};
+int N=3; //3 vertices
+float tx = 50, ty = 50;
+float theta = 30; //in degrees
+float thetar = theta*(M_PI/100); //in radians
+
+//Translation Function
+void translate_tri(){
+glBegin(GL_LINE_LOOP);
+for(int i=0; i<3; i++){
+glVertex2f(tri[0][i]+tx, tri[1][i]+ty);
+}
+glEnd();
+}
+//Rotation Function
+void rotate_tri(){
+glBegin(GL_LINE_LOOP);
+for(int i=0; i<3; i++){
+float x = tri[0][i], y = tri[1][i];
+float xb = x*cos(thetar) - y*sin(thetar);
+float yb = x*sin(thetar) + y*cos(thetar);
+glVertex2f(xb,yb);
+}
+glEnd();
+}
+
+//Draw initial triangle
+void draw_tri(){
+glBegin(GL_LINE_LOOP);
+for(int i=0; i<3; i++){
+glVertex2f(tri[0][i], tri[1][i]);
+}
+glEnd();
+}
+
+void display(){
+glClear(GL_COLOR_BUFFER_BIT);
+glColor3f(1,0,0); //red
+draw_tri();
+//1. Translation
+//glColor3f(1,0,1); //magenta
+//translate_tri();
+//2. Rotation
+rotate_tri();
+glFlush();
+}
+
+void myinit(){
+glClearColor(1,1,1,1);
+glMatrixMode(GL_PROJECTION);
+gluOrtho2D(-250,250,-250,250);
+glMatrixMode(GL_MODELVIEW);
+
+}
+
+int main(int argc, char **argv){
+glutInit(&argc, argv);
+glutInitDisplayMode(GLUT_RGB| GLUT_SINGLE);
+glutInitWindowSize(500,500);
+glutCreateWindow("2D geometric operations");
+myinit();
+glutDisplayFunc(display);
+glutMainLoop();
+return 0;
+}
