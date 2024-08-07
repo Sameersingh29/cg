c697bd381c69b21b1d2d9a923cdcbbe6021c50db
Sameer <73021790+Sameersingh29@users.noreply.github.com>
2024-08-07 20:54:15 +0530

    Update 2D_geom.cpp

--- 2D_geom.cpp
+++ 2D_geom.cpp
@@ -6,6 +6,9 @@ int N=3; //3 vertices
 float tx = 50, ty = 50;
 float theta = 30; //in degrees
 float thetar = theta*(M_PI/100); //in radians
+float px = -100, py = -100; //pivot pt. for rotation
+float sx = 0.5, sy = 0.5; //Scaling factors
+float fx= -100, fy = -100; //Fixed pt. for scaling
 
 //Translation Function
 void translate_tri(){
@@ -23,6 +26,43 @@ float x = tri[0][i], y = tri[1][i];
 float xb = x*cos(thetar) - y*sin(thetar);
 float yb = x*sin(thetar) + y*cos(thetar);
 glVertex2f(xb,yb);
+}
+glEnd();
+}
+//Pivot pt. rotation
+void pivot_rotation(){
+glBegin(GL_LINE_LOOP);
+for(int i=0; i<3; i++){
+float x = tri[0][i], y = tri[1][i];
+float xb = px+(x-px)*cos(thetar) - (y-py)*sin(thetar);
+float yb = py+(x-px)*sin(thetar) + (y-py)*cos(thetar);
+glVertex2f(xb,yb);
+}
+glEnd();
+}
+
+//Scaling about Fixed point
+void fixed_scale(){
+glBegin(GL_LINE_LOOP);
+for(int i=0; i<3; i++){
+float x = tri[0][i] ;
+float y = tri[1][i] ;
+float xb = x*sx + fx*(1-sx);
+float yb = y*sy + fy*(1-sy);
+
+
+glVertex2f(xb, yb);
+}
+glEnd();
+}
+
+//Scaling Function
+void scaling(){
+glBegin(GL_LINE_LOOP);
+for(int i=0; i<3; i++){
+float x = tri[0][i] * sx;
+float y = tri[1][i] * sy;
+glVertex2f(x, y);
 }
 glEnd();
 }
@@ -44,7 +84,14 @@ draw_tri();
 //glColor3f(1,0,1); //magenta
 //translate_tri();
 //2. Rotation
-rotate_tri();
+//rotate_tri();
+//3. Pivot Rotation
+//glColor3f(1,0,1);
+//pivot_rotation();
+//4. 2D scaling
+//scaling();
+//5. Scaling about fixed point
+fixed_scale();
 glFlush();
 }
 
